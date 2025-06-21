1a3f25bef8cd25ecef6ec78275849cec773ba214
Connor Rosewell <rosewcon@amazon.com>
2025-06-21 12:31:38 +0100

    Adding make dir before codegen logic

--- src/graphql/mergeSchemas.mjs
+++ src/graphql/mergeSchemas.mjs
@@ -1,10 +1,11 @@
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeTypeDefs } from "@graphql-tools/merge";
-import { writeFileSync } from "fs";
+import { writeFileSync, mkdirSync } from "fs";
 import { print } from "graphql";
 
 const typesArray = loadFilesSync("../**/*.graphql");
 const mergedTypes = mergeTypeDefs(typesArray, { all: true });
 
+mkdirSync("./src/generated", { recursive: true });
 writeFileSync("./src/generated/combined.graphql", print(mergedTypes));
 console.log("✅ Combined GraphQL schema written to combined.graphql");
